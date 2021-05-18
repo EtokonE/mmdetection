6d597bd9708aed69b97f5645497972c55fe32370
Maksim Kalinin <maksim.kalinin.21@mail.ru>
2021-05-18 11:45:10 +0300

    Update yolo608_base.py

--- my_configs/yolo/yolo608_base.py
+++ my_configs/yolo/yolo608_base.py
@@ -185,8 +185,7 @@ data = dict(
                     dict(type='Collect', keys=['img'])
                 ])
         ],
-        samples_per_gpu=64,
-        workers_per_gpu=2))
+        samples_per_gpu=52,))
 optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0005)
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 lr_config = dict(
