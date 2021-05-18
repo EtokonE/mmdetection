d95042a0d049864b4e54a2ad2b9b3788639f2ac5
EtokonE <maksim.kalinin.21@mail.ru>
2021-05-18 07:12:22 +0000

    yolo_base change test batch size

--- my_configs/yolo/yolo608_base.py
+++ my_configs/yolo/yolo608_base.py
@@ -184,7 +184,8 @@ data = dict(
                     dict(type='ImageToTensor', keys=['img']),
                     dict(type='Collect', keys=['img'])
                 ])
-        ]))
+        ],
+        samples_per_gpu=10,))
 optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0005)
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 lr_config = dict(
