eedced908373e3940fbe81257305509a27f4807e
Maksim Kalinin <maksim.kalinin.21@mail.ru>
2021-05-30 11:08:37 +0300

    Update my_dataset_drop10.py

--- mmdet/datasets/my_dataset_drop10.py
+++ mmdet/datasets/my_dataset_drop10.py
@@ -16,26 +16,26 @@ class MyDataset_drop10(CustomDataset):
 
         data_infos = []
         for i in data:
+                
+            # Переменная для хранения боксов подлежащих удалению
+            pop_list = []
 
-                # Переменная для хранения боксов подлежащих удалению
-                pop_list = []
-
-                # Для каждого изображения необходимо проверить площадь каждого бокса
-                for num_bbox in range(len(i['ann']['bboxes'])):
-                    if abs( (i['ann']['bboxes'][num_bbox][0] - i['ann']['bboxes'][num_bbox][2]) * (i['ann']['bboxes'][num_bbox][1] - i['ann']['bboxes'][num_bbox][3]) ) < 10:
-                        pop_list.append(num_bbox)
+            # Для каждого изображения необходимо проверить площадь каждого бокса
+            for num_bbox in range(len(i['ann']['bboxes'])):
+                if abs( (i['ann']['bboxes'][num_bbox][0] - i['ann']['bboxes'][num_bbox][2]) * (i['ann']['bboxes'][num_bbox][1] - i['ann']['bboxes'][num_bbox][3]) ) < 10:
+                    pop_list.append(num_bbox)
 
                 # Удаляем слишком маленькие боксы и метки их классов
-                for el in pop_list[::-1]:
-                    i['ann']['bboxes'].pop(el)
-                    i['ann']['labels'].pop(el)
+            for el in pop_list[::-1]:
+                i['ann']['bboxes'].pop(el)
+                i['ann']['labels'].pop(el)
         
-                i['ann']['bboxes'] = np.array(i['ann']['bboxes']).astype(np.float32)
-                i['ann']['labels'] = np.array(i['ann']['labels']).astype(np.int64)
+            i['ann']['bboxes'] = np.array(i['ann']['bboxes']).astype(np.float32)
+            i['ann']['labels'] = np.array(i['ann']['labels']).astype(np.int64)
 
-                data_infos.append(i)
+            data_infos.append(i)
 
-            return data_infos
+        return data_infos
 
     def get_ann_infos(self, idx):
         return self.data_infos[idx]['ann']
