8b90fa9fab7fe9552c1512a63dfb439dba0e7957
Maxim Kalinin <maksim.kalinin.2110@gmail.com>
2021-06-03 16:05:44 +0300

    save_model

--- check_model.py
+++ check_model.py
@@ -1,5 +1,22 @@
-from mmcv.cnn import VGG
+from mmdet.apis import init_detector
 import torch
-model = VGG(depth=16)
-print(model)
-torch.save(model, 'model.pth')
+from argparse import ArgumentParser
+
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument('config', help='Config file')
+    parser.add_argument('checkpoint', help='Checkpoint file')
+    parser.add_argument('--device', default='cuda:0', help='Device used for inference')
+    args = parser.parse_args()
+    return args
+    
+def main(args):
+    model = init_detector(args.config, args.checkpoint, device=args.device)
+    print(model)
+    torch.save(model.state_dict(), 'saved_model_state.pt')    
+    print('Model saved')
+
+
+
+
