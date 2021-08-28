d7e20c2909a8fcfc472cbeff943722f48d53a4a2
Maxim Kalinin <maksim.kalinin.2110@gmail.com>
2021-08-28 13:49:21 +0300

    save image in image_demo and create speedTest

--- demo/speedTest_image.py
+++ demo/speedTest_image.py
@@ -1,5 +1,6 @@
 import asyncio
 from argparse import ArgumentParser
+import timeit
 
 from mmdet.apis import (async_inference_detector, inference_detector,
                         init_detector, show_result_pyplot)
@@ -18,18 +19,30 @@ def parse_args():
         '--async-test',
         action='store_true',
         help='whether to set async options for async inference.')
+    parser.add_argument('--speed_test', action='store_true', help='start speed test on one frame')
     args = parser.parse_args()
     return args
 
+args = parse_args()
+model = init_detector(args.config, args.checkpoint, device=args.device)
+img = args.img
 
+'''
+def speed_test():
+    args = parse_args()
+    model = init_detector(args.config, args.checkpoint, device=args.device)
+    img = args.img
+    #return inference_detector(model, img)
+    #print(timeit.repeat('inference_detector(model, img)', 'from __main__ import inference_detector, model, img', repeat=20, number=1))
+    print(timeit.repeat('inference_detector(model, img)', 'from __main__ import inference_detector', repeat=20, number=1))
+'''
 def main(args):
     # build the model from a config file and a checkpoint file
     model = init_detector(args.config, args.checkpoint, device=args.device)
     # test a single image
     result = inference_detector(model, args.img)
     # show the results
-    show_result_pyplot(model, args.img, result, score_thr=args.score_thr)
-    # добавлено сохранение в файл
+    #show_result_pyplot(model, args.img, result, score_thr=args.score_thr)
     model.show_result(args.img, result, score_thr=args.score_thr,font_size=5 , out_file='result_.jpg')
 
 
@@ -47,5 +60,8 @@ if __name__ == '__main__':
     args = parse_args()
     if args.async_test:
         asyncio.run(async_main(args))
+    elif args.speed_test:
+        print(timeit.repeat('inference_detector(model, img)', 'from __main__ import inference_detector, model, img', repeat=20, number=1)
+
     else:
         main(args)
